f446659aef8b72806133d020aef6a6334e332c51
Yandile <yandile@softidoc.co.za>
2024-12-26 14:08:00 +0200

    feat(search): added the search input field and its functionality.

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { reactive } from 'vue'
+import weatherApiService from '@/utils/weatherApiService'
+
+const emit = defineEmits(['place-data'])
+const searchTerm = reactive({
+    query: ``,
+    timeout: null,
+    results: [],
+})
+
+const handleSearch = () => {
+    clearTimeout(searchTerm.timeout)
+    searchTerm.timeout = setTimeout(async () => {
+        if (searchTerm.query !== '') {
+            try {
+                const { data } = await weatherApiService.searchPlaces(searchTerm.query)
+                searchTerm.results = data
+            } catch (error) {
+                console.error('Search error:', error)
+                searchTerm.results = []
+            }
+        } else {
+            searchTerm.results = []
+        }
+    }, 500)
+}
+
+const getWeather = async (id) => {
+    try {
+        const { data } = await weatherApiService.getForecast(id)
+        emit('place-data', data)
+        searchTerm.query = ''
+        searchTerm.results = []
+    } catch (error) {
+        console.error('Forecast error:', error)
+    }
+}
+</script>
+
+<template>
+    <div>
+        <!-- search field -->
+        <form>
+            <div class="bg-white border border-indigo-600/30 rounded-lg shadow-lg flex items-center">
+                <i class="fa-solid fa-magnifying-glass p-2 text-indigo-600"></i>
+                <input type="text" placeholder="Search for a place"
+                    class="rounded-r-lg p-2 border-0 outline-0 focus:ring-2 focus:ring-indigo-600 ring-inset w-full"
+                    v-model="searchTerm.query" @input="handleSearch" />
+            </div>
+        </form>
+        <!-- search suggestions -->
+        <div class="bg-white my-2 rounded-lg shadow-lg">
+            <div v-if="searchTerm.results.length !== null">
+                <div v-for="place in searchTerm.results" :key="place.id">
+                    <button @click="getWeather(place.id)"
+                        class="px-3 my-2 hover:text-indigo-600 hover:font-bold w-full text-left">
+                        {{ place.name }}, {{ place.region }}, {{ place.country }}
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
