f0375d1fe0d0868b64cd7dc17801033b75815981
Yandile <yandile@softidoc.co.za>
2024-12-27 17:32:47 +0200

    feat(app): implement place management with add and delete functionality; add BorderLine component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,45 @@
+<script setup>
+import { ref } from 'vue'
+import SearchInput from './components/SearchInput.vue'
+import WeatherCard from './components/WeatherCard.vue'
+
+const places = ref([])
+
+const addPlace = (data) => {
+  places.value.push(data)
+  // console.log(data)
+}
+
+const deletePlace = (name) => {
+  // console.log(name)
+  if (confirm('Are you sure you want to delete this place?')) {
+    places.value = places.value.filter((place) => place.location.name !== name)
+  }
+}
+</script>
+
 <template>
-  <main>home</main>
+  <!-- Date -->
+
+  <div class="text-center mb-6">
+    {{
+      new Date().toLocaleDateString('en-US', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      })
+    }}
+  </div>
+  <!-- Search  -->
+  <div>
+    <SearchInput @place-data="addPlace" />
+  </div>
+
+  <!-- Weather cards -->
+  <div class="grid grid-cols-2 gap-4">
+    <div v-for="(place, index) in places" :key="index">
+      <WeatherCard :place="place" @delete-place="deletePlace" />
+    </div>
+  </div>
 </template>
