3dee9d7eccafcda8cc1a0cce86e5a5cb8467c55a
Yandile <yandhilemaluleke@gmail.com>
2024-12-29 12:21:35 +0200

    feat(api): configure environment variables and set up API proxy for weather data

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,22 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  define: {
+    'process.env': {},
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://api.weatherapi.com/v1',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
     },
   },
 })
