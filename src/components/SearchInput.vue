8e90c77d2863670f828a9b610c5a4aff9eec6f65
Yandile <yandhilemaluleke@gmail.com>
2025-02-11 21:54:17 +0200

    testing deployment

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -46,7 +46,7 @@ const getWeather = async (id) => {
         <form>
             <div class="bg-white border border-indigo-600/30 rounded-lg shadow-lg flex items-center">
                 <i class="fa-solid fa-magnifying-glass p-2 md:p-3 text-indigo-600"></i>
-                <input type="text" placeholder="Search for a place"
+                <input type="text" placeholder="Search place"
                     class="rounded-r-lg p-2 md:p-3 text-sm md:text-base border-0 outline-0 focus:ring-2 focus:ring-indigo-600 ring-inset w-full"
                     v-model="searchTerm.query" @input="handleSearch" />
             </div>
